ce40725ca618a0ca273a433bce0bae56fd02d781
unknown <makoed.vladislav04@gmail.com>
2024-06-14 01:29:54 +0300

    update

--- static/js/views/Routerax.js
+++ static/js/views/Routerax.js
@@ -32,7 +32,7 @@ const Routerax = async () => {
                             </p>
                         </div>
                         <div class="button-click">
-                            <button class="buttonclick" onclick="location.href='/placing'" data-link>Купить онлайн</button>
+                            <a class="buttonclick" href='/placing' data-link>Купить онлайн</a>
 
                             <button class="buttonclick shop__card--btn" data-product-add="1003">Добавить в корзину</button>
                         </div>
